b0283051b3fddfaea13706a39a6377bb60c7f90c
sangeeth108 <thisaranga108@gmail.com>
2024-08-11 20:48:49 +0530

    create user model

---
+++ Nethu_Education_Institute_Management_System/models/user.js
@@ -0,0 +1,26 @@
+import { Schema, model, models } from 'mongoose';
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      role: {
+        type: String,
+        enum: ['admin', 'teacher', 'student'],
+        default: 'student',
+      },
+})
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
