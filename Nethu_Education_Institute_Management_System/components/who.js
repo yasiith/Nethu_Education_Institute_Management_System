a44e439ada28dc2ba0ea61571d717fd68663546c
sangeeth108 <thisaranga108@gmail.com>
2024-08-11 20:30:42 +0530

    typo correction

--- Nethu_Education_Institute_Management_System/components/who.js
+++ Nethu_Education_Institute_Management_System/components/who.js
@@ -7,7 +7,7 @@ const Who = () => {
     <>
       <div className="container mx-auto ">
         <h1 className='mt-20 mb-6 text-7xl font-bold leading-none tracking-tight text-gray-900 text-clip text-center'>Who we are</h1>
-        <div class='flex'>
+        <div className='flex'>
             <div className="size-80 bg-gray-200"></div>
             <p className='ml-4 text-gray-500 flex'>This is  content to make the page longer</p>
         </div>
