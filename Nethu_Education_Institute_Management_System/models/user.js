34f7d79b78e0ef972ecba189094d867e685bf33c
sangeeth108 <thisaranga108@gmail.com>
2024-08-11 20:55:09 +0530

    Update user.js

--- Nethu_Education_Institute_Management_System/models/user.js
+++ Nethu_Education_Institute_Management_System/models/user.js
@@ -3,16 +3,16 @@ import { Schema, model, models } from 'mongoose';
 const UserSchema = new Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Name is required!']
       },
       email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required'],
         unique: true,
       },
       password: {
         type: String,
-        required: true,
+        required: [true, 'password is required'],
       },
       role: {
         type: String,
