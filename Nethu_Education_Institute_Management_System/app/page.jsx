210ee8f70c2a86e218450c668a5bb40f3ae4c094
YeharaMewan <yehmewan@gmail.com>
2024-08-13 16:34:26 +0530

    routing landing page

--- Nethu_Education_Institute_Management_System/app/page.jsx
+++ Nethu_Education_Institute_Management_System/app/page.jsx
@@ -1,32 +1,16 @@
-import Classes from "@components/classes"
-import Footer from "@components/footer"
-import Navbar from "@components/nav"
-import Teachers from "@components/teachers"
-import Who from "@components/who"
-import ScrollInCounter from "@components/counter" 
-
+import Classes from "@components/classes";
+import Footer from "@components/footer";
+import Navbar from "@components/nav";
+import Teachers from "@components/teachers";
+import Who from "@components/who";
+import ScrollInCounter from "@components/counter";
+import LandingPage from "./LandingPage/page";
 
 const Home = () => {
   return (
     <>
       <div>
-        <section id="home_section"> 
-          <Navbar />
-        </section>
-        <section id="about_section">
-          <Who />
-          <ScrollInCounter/>
-        </section>
-        <section id="classes_section">
-          <Classes />
-        </section>
-        <section id="teachers_section">
-          <Teachers />
-        </section>
-        <section id="contact_section">
-          <Footer/>
-        </section>
-        
+        <LandingPage />
       </div>
     </>
   );
