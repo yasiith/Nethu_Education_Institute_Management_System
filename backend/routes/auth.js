63926a654f60dd44b7f5e7ab40ad685f519c33e6
SahanMihikalpa <mihikalpasahan2001@gmail.com>
2024-10-01 16:45:11 +0530

    delete students

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { registerAdmin, createUser, loginUser, getUser, createStudent, createTeacher } = require('../controllers/authController');
+const User = require('../models/User');
+const { registerAdmin, createUser, loginUser, getUser, createStudent, createTeacher,deleteStudent } = require('../controllers/authController');
 const auth = require('../middleware/auth');
 const checkRole = require('../middleware/checkRole');
 const router = express.Router();
@@ -9,6 +10,7 @@ router.post('/register-admin', registerAdmin);
 router.post('/create-student', auth, checkRole('admin'), createStudent);
 router.post('/create-teacher', auth, checkRole('admin'), createTeacher);
 
+router.delete('/delete-student/:id', auth, checkRole('admin'), deleteStudent);
 // Auth routes
 router.post('/login', loginUser);
 
