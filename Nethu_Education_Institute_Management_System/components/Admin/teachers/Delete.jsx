56c3334171ae2c5eeacb777ce70589974fd1087d
yxsith <148195409+yxsith@users.noreply.github.com>
2024-10-01 16:58:18 +0530

    Modified teacher and student delete component

--- Nethu_Education_Institute_Management_System/components/Admin/teachers/Delete.jsx
+++ Nethu_Education_Institute_Management_System/components/Admin/teachers/Delete.jsx
@@ -15,7 +15,7 @@ const Register = () => {
 
       <div className="flex flex-col md:flex-row justify-center items-center gap-6 max-w-4xl">
         <div className="flex flex-col gap-6 items-center ">
-          <div className="bg-teal-500 w-[350px] h-[300px] rounded-[45px] flex items-center justify-center text-white text-center p-5">
+          <div className="bg-teal-500 w-[350px] h-[200px] rounded-[45px] flex items-center justify-center text-white text-center p-5">
             <div className="flex flex-col leading-none">
                 <p className="text-[40px] font-semibold">DELETE</p>
                 <p className="text-[60px] font-bold">TEACHER</p>
@@ -28,21 +28,7 @@ const Register = () => {
           </button>
         </div>
 
-        <div className="bg-gray-400 w-[400px] p-8 rounded-[40px] flex flex-col gap-4 m-5">
-          <label className="text-white text-lg font-bold">Full Name</label>
-          <input
-            type="text"
-            placeholder="Full Name"
-            className="p-2 rounded-[30px] border-none focus:ring-2 focus:ring-teal-500"
-          />
-
-          <label className="text-white text-lg font-bold">Email</label>
-          <input
-            type="email"
-            placeholder="Email"
-            className="p-2 rounded-[30px] border-none focus:ring-2 focus:ring-teal-500"
-          />
-
+        <div className="bg-gray-400 w-[400px] p-8 rounded-[40px] flex flex-col justify-center gap-4 m-5 h-[300px]">
           <label className="text-white text-lg font-bold">Teacher ID</label>
           <input
             type="text"
