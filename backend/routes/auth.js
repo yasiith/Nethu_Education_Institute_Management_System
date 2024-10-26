30c970074366436d9d8ed218fb7fb916abe0d83e
SahanMihikalpa <mihikalpasahan2001@gmail.com>
2024-10-26 12:27:34 +0530

    user update and teacher login navigation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/User');
 const { registerAdmin, createUser, loginUser, getUser, createStudent, createTeacher,deleteStudent,getStudents } = require('../controllers/authController');
+const { updatestudent} = require('../controllers/userupdate');
 const auth = require('../middleware/auth');
 const checkRole = require('../middleware/checkRole');
 const router = express.Router();
@@ -10,6 +11,9 @@ router.post('/register-admin', registerAdmin);
 router.post('/create-student', auth, checkRole('admin'), createStudent);
 router.post('/create-teacher', auth, checkRole('admin'), createTeacher);
 router.get('/viewstudents', auth, checkRole('admin'), getStudents);
+router.get('/updatesudent',auth,checkRole('admin'),updatestudent);
+
+
 
 router.delete('/delete-student/:id', auth, checkRole('admin'), deleteStudent);
 // Auth routes
@@ -22,6 +26,8 @@ router.get('/user/:id', auth, checkRole('admin'), getUser);
 router.get('/test', (req, res) => {
     res.json({ msg: 'Test route works' });
 });
+
+
 
 
 module.exports = router;
