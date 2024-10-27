faa6a2ec23fc45d93f36ba01717bd7a7d3150714
YeharaMewan <yehmewan@gmail.com>
2024-10-27 14:02:31 +0530

    teacher inside grade

--- Nethu_Education_Institute_Management_System/app/page.jsx
+++ Nethu_Education_Institute_Management_System/app/page.jsx
@@ -8,13 +8,15 @@ import LandingPage from "./LandingPage/page";
 import Dashboard from "@components/Teacher/Dashboard";
 import Page1 from "./teachers/page";
 import Page from "./teachers/gradeCreateForm/page";
-import TeaTeacherNavLeft from "./teachers/insideGrade/page";
+import TeaTeacherNavLeft from "./teachers/insideGrade_/page";
+import Calendar from "@components/Teacher/Calander";
+import InsideGrade_ from "./teachers/insideGrade_/page";
 
 const Home = () => {
   return (
     <>
       <div>
-        <TeaTeacherNavLeft />
+        <LandingPage />
       </div>
     </>
   );
