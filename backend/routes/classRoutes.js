ff4f0b40537d29682334b35683b043137e0a4fa3
SahanMihikalpa <mihikalpasahan2001@gmail.com>
2025-01-06 00:48:53 +0530

    class filtering page for students backend api

--- backend/routes/classRoutes.js
+++ backend/routes/classRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllClasses, createclass,getClassesByTeacher,getFilteredClasses ,getClassDetails } = require('../controllers/classController');
+const { getAllClasses, createclass,getClassesByTeacher,getFilteredClasses ,getClassDetails, Getgradesubject } = require('../controllers/classController');
 
 // Define the GET route for fetching all classes
 router.get('/api/classes', getAllClasses);
@@ -14,8 +14,13 @@ router.post('/api/classes/createclass', createclass);
 // Route to filter classes by subject and grade
 router.get('/api/classes/filter', getFilteredClasses);
 
+//get class subject grade
+router.get('/api/classes/Getgradesubject', Getgradesubject);
+
 // GET /api/classes/:classId - Retrieve class details by class ID
 router.get('/api/classes/:classId', getClassDetails); // Use the controller function here
+
+
 
 
 
