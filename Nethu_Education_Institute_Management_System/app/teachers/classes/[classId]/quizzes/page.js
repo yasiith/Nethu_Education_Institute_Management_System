0f9858036f16e55ff9bce4b4d688389c73301ecc
yasiith <yasithvimukthi259@gmail.com>
2025-01-06 12:46:05 +0530

    Delete Quiz Card Frontend

--- Nethu_Education_Institute_Management_System/app/teachers/classes/[classId]/quizzes/page.js
+++ Nethu_Education_Institute_Management_System/app/teachers/classes/[classId]/quizzes/page.js
@@ -2,6 +2,7 @@
 import Footer from '@components/footer'
 import CreatedQuizCard from '@components/Teacher/Quiz/CreatedQuizCard'
 import CreateNewQuizBtn from '@components/Teacher/Quiz/CreateNewQuizBtn'
+import DeleteQuizCard from '@components/Teacher/Quiz/DeleteQuizCard'
 import QuizNav from '@components/Teacher/Quiz/QuizNav'
 import React from 'react'
 
@@ -11,6 +12,7 @@ const Page = () => {
       <QuizNav />
       <CreatedQuizCard />
       <CreateNewQuizBtn />
+      <DeleteQuizCard /> {/* make this component only visible when there is a quiz */}
       <Footer />
     </div>
   )
